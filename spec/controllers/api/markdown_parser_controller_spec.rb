21a4e02fcae000d22619736a2240e6b7dd271ac0
David Pedersen <david.pdrsn@gmail.com>
2014-11-05 15:16:41 +0100

    Isolate specs for MarkdownParserController

--- spec/controllers/api/markdown_parser_controller_spec.rb
+++ spec/controllers/api/markdown_parser_controller_spec.rb
@@ -1,10 +1,16 @@
 require 'rails_helper'
 
 describe Api::MarkdownParserController do
-  describe '#parse' do
-    it 'parses markdown' do
-      post :parse, markdown: "**hi**"
-      expect(response.body).to include "<strong>hi</strong>"
+  describe "#parse" do
+    it "uses MarkdownParser to parse some markdown and render it" do
+      markdown = "**hi**"
+      parser = double("parser", parse: "<strong>hi</strong>")
+      allow(MarkdownParser).to receive(:new).and_return(parser)
+
+      post :parse, markdown: markdown
+
+      expect(parser).to have_received(:parse).with(markdown)
+      expect(response.body).to eq "<strong>hi</strong>"
     end
   end
 end
