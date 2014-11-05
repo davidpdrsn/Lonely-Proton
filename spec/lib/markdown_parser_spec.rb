c64cb2eac6b17c0b64a2e4b14c75de3e3b5aafd5
David Pedersen <david.pdrsn@gmail.com>
2014-11-05 13:55:21 +0100

    Remove redundant test case

--- spec/lib/markdown_parser_spec.rb
+++ spec/lib/markdown_parser_spec.rb
@@ -5,19 +5,4 @@ describe MarkdownParser do
     parser = MarkdownParser.new
     expect(parser.parse("**hi**")).to include "<strong>hi</strong>"
   end
-
-  it 'parses markdown with fenced code blocks' do
-    parser = MarkdownParser.new
-    html = parser.parse(<<-MARKDOWN)
-```javascript
-[1,2,3].forEach(function(n) {
-  console.log(n);
-});
-```
-    MARKDOWN
-
-    expect(html).to include "code"
-    expect(html).to include 'lang="javascript"'
-    expect(html).to include "pre"
-  end
 end
