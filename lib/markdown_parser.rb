16d8d931acb37fddf7ab19ef56083964b8fbfa4d
David Pedersen <david.pdrsn@gmail.com>
2014-11-05 14:05:01 +0100

    Refactor MarkdownParser

--- lib/markdown_parser.rb
+++ lib/markdown_parser.rb
@@ -1,21 +1,28 @@
 class MarkdownParser
+  OPTIONS = {
+    fenced_code_blocks: true,
+    no_intra_emphasis: true,
+    autolink: true,
+    strikethrough: true,
+    lax_html_blocks: true,
+    superscript: true,
+  }
+
   def parse(text)
-    coderayified = CodeRayify.new(
+    parser.render(text).html_safe
+  end
+
+  private
+
+  def parser
+    Redcarpet::Markdown.new(renderer, OPTIONS)
+  end
+
+  def renderer
+    CodeRayify.new(
       filter_html: true,
       hard_wrap: true,
     )
-
-    options = {
-      fenced_code_blocks: true,
-      no_intra_emphasis: true,
-      autolink: true,
-      strikethrough: true,
-      lax_html_blocks: true,
-      superscript: true,
-    }
-
-    markdown_to_html = Redcarpet::Markdown.new(coderayified, options)
-    markdown_to_html.render(text).html_safe
   end
 end
 
