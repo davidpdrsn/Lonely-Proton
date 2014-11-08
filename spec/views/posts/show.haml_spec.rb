75a283ba6de7ddcce4e4774700bc32f014c150aa
David Pedersen <david.pdrsn@gmail.com>
2014-11-08 15:12:07 +0100

    Show when posts are drafts
    - This makes it easier to see if you're on a draft post

--- spec/views/posts/show.haml_spec.rb
+++ spec/views/posts/show.haml_spec.rb
@@ -24,6 +24,20 @@ describe "posts/show.haml" do
     expect(page).to have_link tag.name
   end
 
+  it "shows if the post is a draft" do
+    post = stubbed_post(published_at: nil)
+    assign(:post, post)
+
+    expect(page).to have_content "(draft)"
+  end
+
+  it "shows when the post is not a draft" do
+    post = stubbed_post(published_at: Time.now)
+    assign(:post, post)
+
+    expect(page).not_to have_content "(draft)"
+  end
+
   def stubbed_post(options = {})
     post = PostWithPrettyDate.new(build_stubbed(:post, options))
     allow(post).to receive(:html).and_return("")
