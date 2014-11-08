ee5d2c98d20202e5b35f20b9d13f4458bf7e89c7
David Pedersen <david.pdrsn@gmail.com>
2014-11-08 14:57:15 +0100

    Add test for showing external links

--- spec/views/posts/show.haml_spec.rb
+++ spec/views/posts/show.haml_spec.rb
@@ -1,8 +1,20 @@
 require "rails_helper"
 
 describe "posts/show.haml" do
-  it "shows a link to the post if its a link"
-  it "shows the linked item if the post is a linked list style post"
+  it "shows a link to the post if its a link" do
+    post = stubbed_post
+    assign(:post, post)
+
+    expect(page).not_to have_css("a.external-link", text: post.title)
+    expect(page).to have_content(post.title)
+  end
+
+  it "shows the linked item if the post is a linked list style post" do
+    post = stubbed_post(link: "google.com")
+    assign(:post, post)
+
+    expect(page).to have_css("a.external-link", text: post.title)
+  end
 
   it "shows a link to the tag" do
     tag = create(:tag)
@@ -10,5 +22,12 @@ describe "posts/show.haml" do
     assign(:post, PostWithPrettyDate.new(post))
 
     expect(page).to have_link tag.name
+  end
+
+  def stubbed_post(options = {})
+    post = PostWithPrettyDate.new(build_stubbed(:post, options))
+    allow(post).to receive(:html).and_return("")
+    allow(post).to receive(:pretty_date).and_return("")
+    post
   end
 end
