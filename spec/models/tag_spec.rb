a67d3b041ffbce15bb565fd9c5919b992dc25c88
David Pedersen <david.pdrsn@gmail.com>
2014-11-09 22:35:04 +0100

    Fix some quotes

--- spec/models/tag_spec.rb
+++ spec/models/tag_spec.rb
@@ -1,18 +1,18 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe Tag do
   it { should have_and_belong_to_many :posts }
   it { should validate_presence_of :name }
   it { should validate_uniqueness_of :name }
 
-  describe '#==' do
-    it 'is == to a decorated tag' do
+  describe "#==" do
+    it "is == to a decorated tag" do
       tag = build :tag
 
       expect(tag == TagWithDomId.new(tag)).to eq true
     end
 
-    it 'is == to the same tag' do
+    it "is == to the same tag" do
       tag = create :tag
 
       expect(tag == tag).to eq true
