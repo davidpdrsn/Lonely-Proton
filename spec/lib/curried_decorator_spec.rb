f63c05710fc4d0aee8ac0be3bae01757fee950f4
David Pedersen <david.pdrsn@gmail.com>
2014-11-09 21:36:08 +0100

    Add missing spec for CurriedDecorator

---
+++ spec/lib/curried_decorator_spec.rb
@@ -0,0 +1,23 @@
+require "rails_helper"
+
+describe CurriedDecorator do
+  it "curries the creation of a decorator with dependencies" do
+    dependency = double("dependency")
+    obj = double("obj")
+    decorator = CurriedDecorator.new(A, dependency: dependency)
+
+    decorated = decorator.new(obj)
+
+    expect(decorated.obj).to eq obj
+    expect(decorated.dependency).to eq dependency
+  end
+end
+
+class A
+  def initialize(obj, dependency:)
+    @obj = obj
+    @dependency = dependency
+  end
+
+  attr_reader :obj, :dependency
+end
