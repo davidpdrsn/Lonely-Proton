9ecaa11d072101a2dde22e209d75be5af16fae33
David Pedersen <david.pdrsn@gmail.com>
2014-11-09 16:22:44 +0100

    Add syntax highlighting test for MarkdownParser

--- spec/lib/markdown_parser_spec.rb
+++ spec/lib/markdown_parser_spec.rb
@@ -1,8 +1,23 @@
-require 'rails_helper'
+require "rails_helper"
 
 describe MarkdownParser do
-  it 'parses markdown' do
+  it "parses markdown" do
     parser = MarkdownParser.new
     expect(parser.parse("**hi**")).to include "<strong>hi</strong>"
+  end
+
+  it "parses and syntax highlights code" do
+    parser = MarkdownParser.new
+
+    code = <<-CODE
+```ruby
+puts "its all good"
+```
+    CODE
+
+    html = parser.parse(code)
+
+    expect(html).to include "class=\"code\""
+    expect(html).to include "pre"
   end
 end
