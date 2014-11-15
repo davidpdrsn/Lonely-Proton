f8bbe25cd1c5ce9ef4ea1b7bd94422b8738e8aba
David Pedersen <david.pdrsn@gmail.com>
2014-11-15 21:48:47 +0100

    Upgrade to ruby 2.1.4

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,21 @@
 # config valid only for Capistrano 3.1
-lock '3.2.1'
+lock "3.2.1"
 
-set :application, 'lonely_proton'
-set :repo_url, 'git@github.com:davidpdrsn/Lonely-Proton.git'
+set :application, "lonely_proton"
+set :repo_url, "git@github.com:davidpdrsn/Lonely-Proton.git"
 set :deploy_to, "/home/deployer/apps/#{fetch :application}"
 set :pty, true
-set :linked_files, %w{config/database.yml config/application.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, ["config/database.yml" "config/application.yml"]
+set :linked_dirs, ["bin", "log", "tmp/pids", "tmp/cache",
+                   "tmp/sockets", "vendor/bundle", "public/system"]
 set :rbenv_type, :user
-set :rbenv_ruby, '2.1.3'
+set :rbenv_ruby, "2.1.4"
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all
 
 namespace :deploy do
-  desc 'Restart application'
+  desc "Restart application"
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
       execute "cd #{current_path} && bundle exec rake RAILS_ENV=#{fetch :stage} assets:precompile --trace"
@@ -33,7 +34,7 @@ namespace :deploy do
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       within release_path do
-        execute :rake, 'cache:clear'
+        execute :rake, "cache:clear"
       end
     end
   end
