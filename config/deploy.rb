99a4444559f832b3d247167b3be36b3d8fe71886
David Pedersen <david.pdrsn@gmail.com>
2014-11-15 23:24:21 +0100

    Fix line that was too long

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,8 @@ set :linked_dirs, ["bin", "log", "tmp/pids", "tmp/cache",
                    "tmp/sockets", "vendor/bundle", "public/system"]
 set :rbenv_type, :user
 set :rbenv_ruby, "2.1.4"
-set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} " \
+  "RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w(rake gem bundle ruby rails)
 set :rbenv_roles, :all
 
