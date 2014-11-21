ba8ebd73a667e29af0fd609cafdb6b0fb65de82f
David Pedersen <david.pdrsn@gmail.com>
2014-11-21 20:02:13 +0100

    Remove existing stat scripts

--- script/stats.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-set -e
-
-if [ -e stats.csv ]; then
-  rm stats.csv
-fi
-
-echo "sources lines of code,test lines of code,test production code ratio, total lines of code,number of files,test time,number of classes" >> stats.csv
-run-command-on-git-revisions $1 $2 "sh stats_on_this_rev.sh" >> stats.csv
