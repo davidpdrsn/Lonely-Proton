be41a38b8d3bc7df0dc53e5a9704d0cc36abc05a
David Pedersen <david.pdrsn@gmail.com>
2014-12-31 14:23:47 +0100

    Show number of files with a given tag

--- spec/features/tags/viewing_all_tags_spec.rb
+++ spec/features/tags/viewing_all_tags_spec.rb
@@ -10,6 +10,20 @@ feature "viewing all tags" do
     expect(page).to have_content "Rails"
   end
 
+  scenario "sees the number of posts associated with the given tag" do
+    rails = create :tag, name: "Rails"
+    create :post, tags: [rails]
+
+    cooking = create :tag, name: "Cooking"
+    create :post, tags: [cooking]
+    create :post, tags: [cooking]
+
+    visit root_path
+    click_link "Tags"
+    expect(page).to have_content "Rails (1 post)"
+    expect(page).to have_content "Cooking (2 posts)"
+  end
+
   scenario "doesn't see tags that have no posts associated with them" do
     create :tag, name: "Cooking"
 
