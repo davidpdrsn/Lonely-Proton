e373dbed9ccc7e47fcdf751c8dcaa649c9fffcf8
David Pedersen <david.pdrsn@gmail.com>
2015-05-18 19:42:56 +0200

    Don't show drafts post in "Best of"

--- app/services/popular_posts_finder.rb
+++ app/services/popular_posts_finder.rb
@@ -1,5 +1,17 @@
 class PopularPostsFinder
   def find_posts
-    Post.all.to_a.sort_by!(&:number_of_views).reverse!.take(5)
+    sort(considered_posts).take(5)
+  end
+
+  private
+
+  def sort(posts)
+    posts.sort_by!(&:number_of_views).reverse!
+  end
+
+  def considered_posts
+    Post
+      .recently_published_first
+      .to_a
   end
 end
--- spec/services/popular_posts_finder_spec.rb
+++ spec/services/popular_posts_finder_spec.rb
@@ -6,7 +6,8 @@ describe PopularPostsFinder do
       one = double("one", number_of_views: 10)
       two = double("two", number_of_views: 5)
       three = double("three", number_of_views: 1)
-      allow(Post).to receive(:all).and_return([two, three, one])
+      allow(Post).to receive(:recently_published_first)
+        .and_return([two, three, one])
 
       finder = PopularPostsFinder.new
 
@@ -16,11 +17,19 @@ describe PopularPostsFinder do
 
     it "doesn't return more than three posts" do
       posts = 10.times.map { double("one", number_of_views: 10) }
-      allow(Post).to receive(:all).and_return(posts)
+      allow(Post).to receive(:recently_published_first).and_return(posts)
 
       finder = PopularPostsFinder.new
 
       expect(finder.find_posts.count).to eq 5
+    end
+
+    it "doesn't return draft posts" do
+      create :post, published_at: nil
+
+      finder = PopularPostsFinder.new
+
+      expect(finder.find_posts.map(&:id)).to eq []
     end
   end
 end
