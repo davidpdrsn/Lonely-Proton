76fe8bb0600bf430a48864ca32f55d87ecd45027
David Pedersen <david.pdrsn@gmail.com>
2015-06-05 03:09:50 +0200

    Change cache config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,8 +51,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, 'cache-1.lonelyproton.com', 'cache-2.lonelyproton.com',
-    { :namespace => LonelyProton, :expires_in => 1.day, :compress => true }
+  config.cache_store = :dalli_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
